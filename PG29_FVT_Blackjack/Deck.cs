abe8eb3398dea1e2248f7836f2e65977e7f12387
Felipe Garcia <pg29felipe@vfs.com>
2025-10-21 15:28:31 -0700

    UPDATE: new method to create the default deck

--- PG29_FVT_Blackjack/Deck.cs
+++ PG29_FVT_Blackjack/Deck.cs
@@ -38,50 +38,146 @@ namespace PG29_FVT_Blackjack
             // Temporary card variable to create new cards
             Card newCard;
             // Loop to create 52 cards and add them to the current deck
-            for (int i = 0; i <= 52; i++)
+            for (int i = 1; i <= 52; i++)
             {
-                // The first 13 cards are of suit 1, the next 13 of suit 2, and so on
-                if (i < 13)
+                if (i <= 13)
                 {
-                    if (i == 0)
+                    if (i == 1)
                     {
-                        newCard = new Card(1, 14, "red");
+                        newCard = new Card(1, 11, "red", "A");
                     }
-                    else
+                    else if (i < 11)
                     {
-                        newCard = new Card(1, i + 1, "red");
+                        newCard = new Card(1, i + 1, "red", $"{i + 1}");
+                    }
+                    else if (i == 11)
+                    {
+                        newCard = new Card(1, 10, "red", "J");
+                    }
+                    else if (i == 12)
+                    {
+                        newCard = new Card(1, 10, "red", "Q");
+                    }
+                    else if (i == 13)
+                    {
+                        newCard = new Card(1, 10, "red", "K");
                     }
                 }
-                else if (i < 26)
+                else if (i <= 26)
                 {
-                    if (i == 13)
+                    if (i == 14)
                     {
-                        newCard = new Card(2, 14, "red");
+                        newCard = new Card(2, 11, "red", "A");
                     }
-                    else
+                    else if (i < 24)
                     {
-                        newCard = new Card(2, i - 12, "red");
+                        newCard = new Card(2, i + 1, "red", $"{i + 1}");
+                    }
+                    else if (i == 24)
+                    {
+                        newCard = new Card(2, 10, "red", "J");
+                    }
+                    else if (i == 25)
+                    {
+                        newCard = new Card(2, 10, "red", "Q");
+                    }
+                    else if (i == 26)
+                    {
+                        newCard = new Card(2, 10, "red", "K");
                     }
                 }
-                else if (i < 39)
+                else if (i <= 39)
                 {
-                    if (i == 26)
+                    if (i == 27)
                     {
-                        newCard = new Card(3, 14, "red");
+                        newCard = new Card(3, 11, "black", "A");
                     }
-                    else
+                    else if (i < 37)
                     {
-                        newCard = new Card(3, i - 25, "red");
+                        newCard = new Card(3, i + 1, "black", $"{i + 1}");
+                    }
+                    else if (i == 37)
+                    {
+                        newCard = new Card(3, 10, "black", "J");
+                    }
+                    else if (i == 38)
+                    {
+                        newCard = new Card(3, 10, "black", "Q");
+                    }
+                    else if (i == 39)
+                    {
+                        newCard = new Card(3, 10, "black", "K");
                     }
                 }
                 else
                 {
-                    newCard = new Card(4, i - 38, "red");
+                    if (i == 40)
+                    {
+                        newCard = new Card(4, 11, "black", "A");
+                    }
+                    else if (i < 50)
+                    {
+                        newCard = new Card(4, i + 1, "black", $"{i + 1}");
+                    }
+                    else if (i == 50)
+                    {
+                        newCard = new Card(4, 10, "black", "J");
+                    }
+                    else if (i == 51)
+                    {
+                        newCard = new Card(4, 10, "black", "Q");
+                    }
+                    else if (i == 52)
+                    {
+                        newCard = new Card(4, 10, "black", "K");
+                    }
                 }
-
-                // Adding the newly created card to the current deck
-                currentDeck.Add(newCard);
             }
+
+            //for (int i = 0; i <= 52; i++)
+            //{
+            //    // The first 13 cards are of suit 1, the next 13 of suit 2, and so on
+            //    if (i < 13)
+            //    {
+            //        if (i == 0)
+            //        {
+            //            newCard = new Card(1, 14, "red");
+            //        }
+            //        else
+            //        {
+            //            newCard = new Card(1, i + 1, "red");
+            //        }
+            //    }
+            //    else if (i < 26)
+            //    {
+            //        if (i == 13)
+            //        {
+            //            newCard = new Card(2, 14, "red");
+            //        }
+            //        else
+            //        {
+            //            newCard = new Card(2, i - 12, "red");
+            //        }
+            //    }
+            //    else if (i < 39)
+            //    {
+            //        if (i == 26)
+            //        {
+            //            newCard = new Card(3, 14, "red");
+            //        }
+            //        else
+            //        {
+            //            newCard = new Card(3, i - 25, "red");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        newCard = new Card(4, i - 38, "red");
+            //    }
+
+            //    // Adding the newly created card to the current deck
+            //    currentDeck.Add(newCard);
+            //}
         }
 
         // Function to create an empty deck of a given size
